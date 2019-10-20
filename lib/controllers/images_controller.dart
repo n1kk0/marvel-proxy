fc481cb884104f0737526472693c15e3080c60fe
n1kk0 <nicolas.moussikian@gmail.com>
2019-10-20 21:18:05 +0200

    change image no available display

--- lib/controllers/images_controller.dart
+++ lib/controllers/images_controller.dart
@@ -8,18 +8,24 @@ class ImagesController extends ResourceController {
 
   final Cache<String, List<int>> _cache;
   final ApplicationMessageHub _messageHub;
+  final List<String> _notAvailables = [
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg",
+    "http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708.gif",
+  ];
 
   @Operation.get()
   Future<Response> getCharacters(@Bind.query("uri") String uri) async {
     List<int> bodyBytes;
 
-    if (_cache.containsKey(uri)) {
-      bodyBytes = _cache.get(uri);
+    final String finalUri = _notAvailables.where((notAvailableUri) => notAvailableUri == uri).isNotEmpty ? "http://www.startupdelta.org/wp-content/uploads/2018/04/No-profile-LinkedIn-600x600.jpg" : uri;
+
+    if (_cache.containsKey(finalUri)) {
+      bodyBytes = _cache.get(finalUri);
     } else {
-      final http.Response response = await http.Client().get(Uri.parse(uri));
+      final http.Response response = await http.Client().get(Uri.parse(finalUri));
       bodyBytes = response.bodyBytes;
       _cache.set("uri", bodyBytes);
-      _messageHub.add({"type": "images", "url": uri, "response": bodyBytes});
+      _messageHub.add({"type": "images", "url": finalUri, "response": bodyBytes});
     }
 
     final File image = MemoryFileSystem().file('tmp')..writeAsBytesSync(bodyBytes);
