5e07a32e93ee570f2cf321b361db422850537906
n1kk0 <nicolas.moussikian@gmail.com>
2019-11-02 09:17:16 +0100

    add resourceUri to marvel api used data

--- lib/models/marvel_character.dart
+++ lib/models/marvel_character.dart
@@ -1,25 +1,28 @@
 class MarvelCharacter {
-  MarvelCharacter({this.name, this.thumbnail});
+  MarvelCharacter({this.name, this.thumbnail, this.resourceUri});
 
   factory MarvelCharacter.fromJson(Map<String, dynamic> json) {
     return MarvelCharacter(
       name: json["name"].toString(),
       thumbnail: "${json["thumbnail"]["path"]}.${json["thumbnail"]["extension"]}",
+      resourceUri: json["resourceUri"].toString(),
     );
   }
 
   String name;
   String thumbnail;
+  String resourceUri;
 
   Map<String, dynamic> toJson() {
     return {
       "name": name,
       "thumbnail": thumbnail,
+      "resourceUri": resourceUri
     };
   }
 
   @override
   String toString() {
-    return "name:$name thumbnail:$thumbnail";
+    return "name:$name thumbnail:$thumbnail resourceUri:$resourceUri";
   }
 }
